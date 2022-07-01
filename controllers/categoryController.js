f4e1335c4e99a3bafcf49ea90528632156092f5c
Jonathan <joohny_2014@hotmail.com>
2022-07-01 20:20:24 -0300

    Function delete added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -38,21 +38,36 @@ module.exports = class CategoryController {
             const index = await Category.findAll()
 
             res.status(200).send(index)
+
         } catch(err) {
             console.log(err)
         }
     }
-    
+
     static async update(req, res) {
 
         const update = req.body
         const id = req.params.id
+        
         try {
             const updated = await Category.update(update, { where: {id}})
 
             res.status(200).send(update)
+
         } catch(err) {
             console.log(err)
+        }
+    }
+
+    static async delete(req, res) {
+
+        const id = req.params.id
+        try {
+            const deleted = await Category.destroy({where: {id}})
+
+            res.status(200).json({message: `Categoria deletada`})
+        } catch(err) {
+        console.log(err)
         }
     }
 }
