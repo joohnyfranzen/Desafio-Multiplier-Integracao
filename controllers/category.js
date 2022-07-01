138451967e1643a0d90ef60a98b02acabdeb172e
Jonathan <joohny_2014@hotmail.com>
2022-07-01 15:11:30 -0300

    feature, function create

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,12 @@
 const Category = require('../models/category')
 
-module.exports = class CategoryController {}
+module.exports = class CategoryController {
+
+    static async createCategory(req, res) {
+        const {codigo, titulo, status} = req.body
+
+        const category = [codigo, titulo, status]
+
+        const createdCategory = await Category.create(category)
+    }
+}
