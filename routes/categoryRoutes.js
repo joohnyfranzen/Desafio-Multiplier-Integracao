c25096eca5f3ea6c26199a075efaf9078568af09
Jonathan <joohny_2014@hotmail.com>
2022-07-01 19:47:28 -0300

    Endpoint index added

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const CategoryController = require('../controllers/categoryController')
 
+router.get('/categorias', CategoryController.index)
 router.get('/categorias/:id', CategoryController.show)
 router.post('/categorias', CategoryController.store)
 
