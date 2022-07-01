c975f7783e39324ae7e6117e1bc474a46929c95e
Jonathan <joohny_2014@hotmail.com>
2022-07-01 16:39:45 -0300

    categoryRoutes added and imported

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const conn = require('./db/conn');
 
-const app = express();
+const app = express()
+
+const categoryRoutes = require('./routes/categoryRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json());
+
+// Routes
+app.use('/', categoryRoutes)
 
 conn.sync().then(() => {
     app.listen(3000)
