e73e3a3963ee467c5f1238df426d5b481b24c9a6
Jonathan <joohny_2014@hotmail.com>
2022-07-01 19:38:53 -0300

    Endpoint show router

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const CategoryController = require('../controllers/categoryController')
 
+router.get('/categorias/:id', CategoryController.show)
 router.post('/categorias', CategoryController.store)
+
 
 module.exports = router
