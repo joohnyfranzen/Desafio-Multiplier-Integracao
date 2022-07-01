3640ab16afead0fcfeae38936ce9addf29be24e9
Jonathan <joohny_2014@hotmail.com>
2022-07-01 14:53:17 -0300

    feature, codigo

--- models/category.js
+++ models/category.js
@@ -3,11 +3,17 @@ const { Datatypes } = require('sequelize')
 const db = require('../db/conn')
 
 const Category = db.define('Category', {
+    codigo: {
+        type: Datatypes.STRING,
+        allowNull: false,
+        required: true,
+    },
     titulo: {
         type: Datatypes.STRING,
         allowNull: false,
         required: true,
-    },    
+    },
+
 }) 
 
 module.exports = Category
