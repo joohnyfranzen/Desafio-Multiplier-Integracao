964b747555a173a3fde50df6074d63ddb5a0079c
Jonathan <joohny_2014@hotmail.com>
2022-07-02 17:48:55 -0300

    Controller, function show

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,5 +2,17 @@ const Inventory = require('../models/inventory')
 
 module.exports = class InventoryController {
 
+    static async show(req, res) {
+
+        const id = req.params.id
+
+        try {
+            const show = await Inventory.findOne({where: {id}})
+
+            res.status(200).json(show)
+        
+        } catch(err) {
+            console.log(err)
+        }
     }
 }
