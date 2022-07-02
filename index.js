800375bb8c9f90fac8695103788c62b8dfcad3f5
Jonathan <joohny_2014@hotmail.com>
2022-07-02 16:11:13 -0300

    index, adding routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const conn = require('./db/conn');
 const app = express()
 
 const categoryRoutes = require('./routes/categoryRoutes')
+const productRoutes = require('./routes/productRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use('/', categoryRoutes)
+app.use('/', productRoutes)
 
 conn.sync().then(() => {
     app.listen(3000)
