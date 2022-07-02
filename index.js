f7f252b4363873036f553a2cf2cace20235554be
Jonathan <joohny_2014@hotmail.com>
2022-07-02 18:04:53 -0300

    inventoryRoutes added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 const categoryRoutes = require('./routes/categoryRoutes')
 const productRoutes = require('./routes/productRoutes')
+const inventoryRoutes = require('./routes/inventoryRoutes')
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use('/', categoryRoutes)
 app.use('/', productRoutes)
+app.use('/', inventoryRoutes)
 
 conn.sync().then(() => {
     app.listen(3000)
