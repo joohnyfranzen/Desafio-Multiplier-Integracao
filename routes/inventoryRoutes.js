6cf02d0bd4160034efabd6f4a78b6a19ad035c9b
Jonathan <joohny_2014@hotmail.com>
2022-07-02 17:55:45 -0300

    Endpoint, function update, and fix / .show

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const InventoryController = require('../controllers/inventoryController')
 
-router.get('/produtos:id/estoque', InventoryController.show)
+router.get('/produtos/:id/estoque', InventoryController.show)
+router.patch('/produtos/:id/estoque', InventoryController.update)
 
 module.exports = router
