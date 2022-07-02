886c33ccfae6acb0843c2cd9855f6bb84bdd3749
Jonathan <joohny_2014@hotmail.com>
2022-07-02 15:55:16 -0300

    Endpoint, function delete

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,5 +7,6 @@ router.get('/produtos', productController.index)
 router.get('/produtos/:id', productController.show)
 router.post('/produtos', productController.store)
 router.patch('/produtos/:id', productController.update)
+router.delete('produtos/:id', productController.delete)
 
 module.exports = router
