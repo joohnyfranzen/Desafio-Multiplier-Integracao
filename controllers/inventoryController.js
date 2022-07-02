114d61f070d82c4dee0435bc01c588b5f202fc8c
Jonathan <joohny_2014@hotmail.com>
2022-07-02 17:35:42 -0300

    ControllerInventory, function store

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,5 +1,19 @@
 const Inventory = require('../models/inventory')
 
 module.exports = class InventoryController {
+
+    static async store(req, res) {
+
+        const {idProduto, quantidade, reserva, status} = req.body
+        
+        const createInventory = {idProduto, quantidade, reserva, status}
     
+        try {
+            const createdInventory = await Inventory.create(createInventory)
+
+            res.status(202).json(createdInventory)
+        } catch(err) {
+            console.log(err)
+        }
+    }
 }
