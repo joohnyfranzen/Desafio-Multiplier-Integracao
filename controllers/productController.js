14d3c53aaac7c516f134fc1f808ec93c26e9d21d
Jonathan <joohny_2014@hotmail.com>
2022-07-02 15:41:14 -0300

    productController, function show

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,17 +4,29 @@ module.exports = class ProductController {
 
     static async store(req, res) {
 
-            const {idCategoria, codigo, nome, decricao, valor, status} = req.body
+        const {idCategoria, codigo, nome, decricao, valor, status} = req.body
     
-            const product = {idCategoria, codigo, nome, decricao, valor, status}
+        const product = {idCategoria, codigo, nome, decricao, valor, status}
 
-            try {
-                const createdProduct = await Product.create(product)
+        try {
+            const createdProduct = await Product.create(product)
 
-                res.status(202).json(createdProduct)
-            } catch(err) {
-                console.log(err)
-            }
+            res.status(202).json(createdProduct)
+        } catch(err) {
+            console.log(err)
         }
-        
+    }
+
+    static async show(req, res) {
+
+        const id = req.params
+
+        try {
+            const show = await Product.findOne({where: {id}})
+
+            res.status(202).json(show)
+        } catch(err) {
+            console.log(err)
+        }
+    }     
 }
