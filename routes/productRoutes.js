80f0d41f6e083d7303533e351ccb3215a64ed882
Jonathan <joohny_2014@hotmail.com>
2022-07-02 15:44:58 -0300

    Endpoint, function index

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const productController = require('../controllers/productController')
 
+router.get('/produtos', productController.index)
 router.get('/produtos/:id', productController.show)
 router.post('/produtos', productController.store)
 
