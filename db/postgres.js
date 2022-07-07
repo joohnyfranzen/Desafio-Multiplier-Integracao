72811f776fa9a2f7779d7657382c576cc8557385
Jonathan <joohny_2014@hotmail.com>
2022-07-07 20:38:21 -0300

    Postgree packages

--- db/postgres.js
+++ db/postgres.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(  "postgres://postgres@localhost:5433/multiplier", 
-  {
-    dialect: "postgres",
-    password: "123",
+const sequelize = new Sequelize(  'postgres://postgres@localhost:5433/multiplier', {
+    dialect: 'postgres',
+    password: '123',
     logging: false,
   }
 );
