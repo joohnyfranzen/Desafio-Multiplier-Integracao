ca0eacc58b126de961adf72eeab17300ba8d0443
Jonathan <joohny_2014@hotmail.com>
2022-07-07 20:48:04 -0300

    Config transfer to postgres

--- config.js
+++ config.js
@@ -4,12 +4,14 @@ const InventoryMysql = require('./models/inventory').InventoryMysql
 const InventoryPostgres = require('./models/inventory').InventoryPostgres
 const CategoryMysql = require('./models/category').CategoryMysql
 const CategoryPostgres = require('./models/category').CategoryPostgres
-
 const mysql = require('./db/mysql')
-const postgres = require('./db/postgres')
+
+
 
 async function transferData(table, models) {
     const Data = await mysql.query(`SELECT * from multiplier`)
+
+
     console.log(`Data`)
     
 }
