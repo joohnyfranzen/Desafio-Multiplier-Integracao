fb7ef275aab5951a96c3e64ea7534d37013a2264
Jonathan <joohny_2014@hotmail.com>
2022-07-09 15:37:11 -0300

    config, category backup

--- config.js
+++ config.js
@@ -5,15 +5,35 @@ const InventoryPostgres = require('./models/inventory').InventoryPostgres
 const CategoryMysql = require('./models/category').CategoryMysql
 const CategoryPostgres = require('./models/category').CategoryPostgres
 const mysql = require('./db/mysql')
+const postgres = require('./db/postgres')
 
 
+mysql.sync().then(() => {
+postgres.sync().then(() => {   
+                           
+async function transferDataCategory() {
+    try {
+        const categories = await CategoryMysql.findAll()
+        
+        var sql = `INSERT INTO public."Categories" ("codigo", "titulo", "status", "createdAt", "updatedAt") VALUES `
 
-async function transferData(table, models) {
-    const Data = await mysql.query(`SELECT * from multiplier`)
+        Category = categories.map((categoria) => {
+            sql += `(${categoria.codigo}, '${categoria.titulo}', ${categoria.status}, '${categoria.createdAt.toISOString().slice(0, 19).replace('T', ' ')}', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}'),`
+        })
+        sql = sql.slice(0, -1);
+        sql += ';';
+        postgres.query(sql, function (err, result) {
+            if (err) throw err
+        })
+        console.log('Produtos  atualizados no postgres')
+    } catch(err) {
+        console.log(err)
+    }
+    }
+transferDataCategory();
 
+   
 
-    console.log(`Data`)
-    
-}
+})
+})
 
-console.log('Aplicação em andamento')
